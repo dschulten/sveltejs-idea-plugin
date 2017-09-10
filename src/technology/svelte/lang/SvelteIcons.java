1b5856785631856f15033e66814deaa1ae37bf58
dschulten <ds@escalon.de>
2017-09-10 17:28:37 +0200

    removed support for .svelte files for now, html is the preferred file extension

--- src/technology/svelte/lang/SvelteIcons.java
+++
@@ -1,9 +0,0 @@
-package technology.svelte.lang;
-
-import com.intellij.openapi.util.IconLoader;
-
-import javax.swing.*;
-
-public class SvelteIcons {
-    static final Icon FILE = IconLoader.getIcon("/icons/svelte.png");
-}
